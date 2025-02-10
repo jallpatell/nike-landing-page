2b21d997fe430856e1d930d8737a391e07fad68c
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-02-11 05:13:44 +0530

    final commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,19 @@
 import './Hero.css'
-const Hero = () => (
+const Hero = () => 
         <main className="hero container">
             <div className="hero-content">
                 <h1>YOUR FEET DESERVES THE BEST </h1>
-                <p>Just Do IT! Your feet deserves the best and we are here to help you with out shoes.Move, Shop, Customise & Celebrate With Us
+                <p>Just Do IT! Your feet deserves the best and we are here to help you with out shoes. Move, Shop, Customise & Celebrate With Us
                 No matter what you feel like doing today, It’s better as a Member.</p>
                 
                 <div className='buttons-shop'>
-                    <button id='button1'> Shop </button>
-                    <button id='button2'> Join Us </button>
+                    <button id='button2'> Become a member! </button>
+                    <p>                         </p>
+                    <button id='button1'> Shop </button> 
                 </div>
 
                 <div className="Shopping">
-                    Available on
+                    Also available at <h> </h>
                     <img className='amazon' src="https://s3-alpha-sig.figma.com/img/9669/ba8f/4f98c0bc6952e9281673372fcb16cfc0?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Ku1Xwwv9mG2HUanbs7DWVNgie1aGZeZLw~w2uYcUSpTPo~E9gYpKwIFdQy49ipIcfme0dEJ0o0~CQNDc4yii96TYV2epo-xF0v0fGUta18Au80ocHb3Cj-rmKljKAy-fxpUBMVYlaSDCSD6jTWwNdS3ObUMeyQ~kremMFiQ1YKU7LrIFpB4LsOfEGBt~gsER7~bpn2fWu4jhHDuVbDRMlhBYpXhtbctCRd0FoqjpS5~42yQPyIaUNRsPlB6JB7r0M9QIaX6kHS6hAUvLeMPhQgEYJNNF19oQvQB38zrmd0tPQ2U31WH~k47n76bxkqMTW5dwWDkByTUlPEh2eJkbvg__" alt="amazon" />
                     <img className='flipcart'  src="https://s3-alpha-sig.figma.com/img/4331/d1d3/fe15236a1a9843e35f7b3b8467e998ff?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nLAjbtisBzdzGvzOuyTVfSLanalye11rWduDHHMZgDUwSAdFrkT0tNrj9BuRpdY7C7YrU244q-4xo1IQvRnFH3i3rffYsdGg8mn~Vc8-i60p2XvUgFmSDAGlDvla-3BNXRBi94PjjwO0xEarYX5Bxw9kDSykBTg~RU1JyR26FPNPeQp~5SdEXAb7YKAZAwi8bz5MYITdAFIXR0udBmPaQDHugpuzd6D79ccS9WtOco48NwJYMbDwBTTCNJPIJ5NJqFjra~7fVmBM~4oCiOzEuMTKWbZ2-7XiRO-DQlVXZbPzcd-zV55gyniHry20ucAYQTwESrOMgWJbJzhc19MUZA__" alt="flipcart" />
                 </div>
@@ -23,6 +24,6 @@ const Hero = () => (
                 <img className='shoe' src='https://m.media-amazon.com/images/I/713C4wFx0jL._SX695_.jpg' alt="shoe" />
             </div>
         </main>
-)
+
 
 export default Hero; 
