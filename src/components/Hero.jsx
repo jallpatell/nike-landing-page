54a0ab8d8b0957a3a42f4f36958e5e0787b3e059
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-02-19 08:42:50 +0530

    -a

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () =>
 
                 <div className="Shopping">
                     Also available at 
-                    <p></p>
+                    <p>     </p>
                 </div>
                     <img className='amazon' src="https://s3-alpha-sig.figma.com/img/9669/ba8f/4f98c0bc6952e9281673372fcb16cfc0?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Ku1Xwwv9mG2HUanbs7DWVNgie1aGZeZLw~w2uYcUSpTPo~E9gYpKwIFdQy49ipIcfme0dEJ0o0~CQNDc4yii96TYV2epo-xF0v0fGUta18Au80ocHb3Cj-rmKljKAy-fxpUBMVYlaSDCSD6jTWwNdS3ObUMeyQ~kremMFiQ1YKU7LrIFpB4LsOfEGBt~gsER7~bpn2fWu4jhHDuVbDRMlhBYpXhtbctCRd0FoqjpS5~42yQPyIaUNRsPlB6JB7r0M9QIaX6kHS6hAUvLeMPhQgEYJNNF19oQvQB38zrmd0tPQ2U31WH~k47n76bxkqMTW5dwWDkByTUlPEh2eJkbvg__" alt="amazon" />
                     <img className='flipcart'  src="https://s3-alpha-sig.figma.com/img/4331/d1d3/fe15236a1a9843e35f7b3b8467e998ff?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nLAjbtisBzdzGvzOuyTVfSLanalye11rWduDHHMZgDUwSAdFrkT0tNrj9BuRpdY7C7YrU244q-4xo1IQvRnFH3i3rffYsdGg8mn~Vc8-i60p2XvUgFmSDAGlDvla-3BNXRBi94PjjwO0xEarYX5Bxw9kDSykBTg~RU1JyR26FPNPeQp~5SdEXAb7YKAZAwi8bz5MYITdAFIXR0udBmPaQDHugpuzd6D79ccS9WtOco48NwJYMbDwBTTCNJPIJ5NJqFjra~7fVmBM~4oCiOzEuMTKWbZ2-7XiRO-DQlVXZbPzcd-zV55gyniHry20ucAYQTwESrOMgWJbJzhc19MUZA__" alt="flipcart" />
